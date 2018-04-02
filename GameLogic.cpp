a6a21518b7f0b5d0b305b46697852f8a884b5028
ccyyhlg <34642551+ccyyhlg@users.noreply.github.com>
2018-04-02 15:21:33 +0800

    Update GameLogic.cpp

--- GameLogic.cpp
+++ GameLogic.cpp
@@ -1,535 +1,60 @@
-#include "StdAfx.h"
-#include "GameLogic.h"
-#include <stdlib.h>
-#include <time.h>
-#include <assert.h>
-//////////////////////////////////////////////////////////////////////////
+#define			NORMAL_TYPE				0				//Ê≠£Â∏∏ÁâåÂûã
+#define			THREEFLUSH				1				//‰∏âÂêåËä±
+#define			THREESTRIGHT			2				//‰∏âÈ°∫Â≠ê
+#define			SIXPAIRS				3				//6ÂØπ
+#define			FIVEPAIRSANDTHREE		4				//5ÂØπ3Êù°
+#define			FOUR_THREE				5				//4ÁªÑ‰∏âÊù°
+#define			ALLSAMECOLOR			6				//Âáë‰∏ÄËâ≤
+#define			ALLLOW					7				//ÂÖ®Â∞è
+#define			ALLHIGH					8				//ÂÖ®Â§ß
+#define			THREE_BOMB				9				//‰∏âÂàÜÂ§©‰∏ã(4‰∏™ÁÇ∏)
+#define			THREE_STRIGHTFLUSH		10				//‰∏âÂêåËä±È°∫
+#define			ROYALCARD				11				//ÂçÅ‰∫åÁöáÊóè
+#define			LOWDRAGON				12				//‰∏ÄÊù°Èæô
+#define			BLACKDRAGON				13				//ÈùíÈæô
 
-//æ≤Ã¨±‰¡ø
+#define			CT_INVALIDCARD				0x00			//Áõ∏ÂÖ¨
+#define			CT_HIGHCARD					0x01			//‰πåÈæô
+#define			CT_ONEPAIR					0x02			//ÂØπÂ≠ê	
+#define			CT_TWOPAIRS					0x03			//‰∏§ÂØπ
+#define			CT_THREE					0x04			//‰∏âÂº†
+#define			CT_STRIGHT					0x05			//È°∫Â≠ê
+#define			CT_FLUSH					0x06			//ÂêåËä±
+#define			CT_FULLHOUSE				0x07			//Ëë´Ëä¶
+#define			CT_FOUR						0x08			//ÈìÅÊîØÔºàÁÇ∏Ôºâ
+#define			CT_FLUSHSTRIGHT				0x09			//ÂêåËä±È°∫
+#define			CT_THREE_FIRST				0x0a			//‰∏âÂÜ≤
+#define			CT_FULLHOUSE_MID			0x0b			//‰∏≠Â¢©Ëë´Ëä¶
 
-//∆ÀøÀ ˝æ›
-const BYTE	CGameLogic::m_bCardListData[52] =
-{
-	0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x01,	//∑ΩøÈ 2 - K A
-	0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x11,	//√∑ª® 2 - K A
-	0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x21,	//∫ÏÃ“ 2 - K A
-	0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x31,	//∫⁄Ã“ 2 - K A	
-};
+#define SELECTTHREE(st) for(int va1=14;va1>1;va1--){ \
+for (int j = 0; j < 4; j++){\
+if ((h[va1] & C34[j]) == C34[j]){\
+	int alco = C34[j]; \
+	std::list<BYTE> ust; \
+	int col = 0; \
+	while (alco){\
+	if (alco % 2 != 0){ ust.push_back((col << 4) + (va1 == 14 ? 1 : va1)); } \
+	col++; \
+	alco = alco >> 1; \
+	} \
+	DeleteUsedCards(card, ust, CurSeq, Layer, st);
 
-//////////////////////////////////////////////////////////////////////////
+#define ENDSELECTTHREE(st) RecallCards(card,ust,CurSeq, Layer, st);}}}
 
-//ππ‘Ï∫Ø ˝
-CGameLogic::CGameLogic()
-{
-	m_iCurIdx = 0;
-}
+#define SELECTPAIR(st) for(int va2=14;va2>1;va2--){ \
+	for (int y = 0; y < 6; y++){\
+	if ((h[va2] & C24[y]) == C24[y] && va2 != va1){\
+		int allco = C24[y]; \
+		std::list<BYTE> ustw; \
+		int co = 0; \
+		while (allco){\
+		if (allco % 2 != 0){ ustw.push_back((co << 4) + (va2 == 14 ? 1 : va2)); } \
+		co++; \
+		allco = allco >> 1; \
+		} \
+		DeleteUsedCards(card, ustw, CurSeq, Layer, st);
 
-//Œˆππ∫Ø ˝
-CGameLogic::~CGameLogic()
-{
-}
-
-
-int Random2Value(int val)
-{
-	int n = abs(rand() + (int)time(NULL));
-	if (val > 0)
-		return abs(n%val);
-	return n;
-}
-//ªÏ¬“∆ÀøÀ
-void CGameLogic::RandCardList(BYTE bCardBuffer[], BYTE bBufferCount)
-{
-	//ªÏ¬“◊º±∏
-	BYTE bCardData[sizeof(m_bCardListData)];
-	memcpy(bCardData, m_bCardListData, sizeof(m_bCardListData));
-
-	//ªÏ¬“∆ÀøÀ
-	BYTE bRandCount = 0, bPosition = 0;
-	do
-	{
-		bPosition = Random2Value(bBufferCount - bRandCount);
-		bCardBuffer[bRandCount++] = bCardData[bPosition];
-		bCardData[bPosition] = bCardData[bBufferCount - bRandCount];
-	} while (bRandCount < bBufferCount);
-
-	return;
-}
-
-int CGameLogic::CompareCardType(int type1, int type2){
-	int sign = 1, maxtype = type1;
-	int re = 0;
-
-	if (type1 < type2){ sign = -1; maxtype = type2; }
-
-	if		(maxtype == THREEFLUSH)			{ re = 3; }
-	else if (maxtype == THREESTRIGHT)		{ re =4; }
-	else if (maxtype == SIXPAIRS)			{ re = 4; }
-	else if (maxtype == FIVEPAIRSANDTHREE)	{ re = 5; }
-	else if (maxtype == FOUR_THREE)			{ re = 6; }
-	else if (maxtype == ALLSAMECOLOR)		{ re = 10; }
-	else if (maxtype == ALLLOW)				{ re = 10; }
-	else if (maxtype == ALLHIGH)			{ re = 10; }
-	else if (maxtype == THREE_BOMB)			{ re = 20; }
-	else if (maxtype == THREE_STRIGHTFLUSH)	{ re = 20; }
-	else if (maxtype == ROYALCARD)			{ re = 24; }
-	else if (maxtype == LOWDRAGON)			{ re = 36; }
-	else if (maxtype == BLACKDRAGON)		{ re = 108; }
-
-	return re*sign;
-}
-
-void DeleteSortedCards(BYTE So[], BYTE Ori[], int sorted){
-	int oc = 13 - sorted;
-	for (int i = 0; i < sorted; i++){
-		for (int j = 0; j < 13; j++){
-			if (So[i] == Ori[j]){
-				BYTE Cu[13] {};
-				int po = j + 1;
-				int c = 13 - po;
-				memcpy(Cu, Ori + po, c);
-				memcpy(Ori + po - 1, Cu, c);
-				break;
-			}
-		}
-	}
-	memset(Ori + oc, 0, sorted);
-}
-
-//ïrÈg§‚§¶«–§Ï§¿
-//2018.3.14  Stephen William Hawking R.I.P
-void CGameLogic::SortCards(BYTE cards[], int STYPE){
-	int v[13] {};
-	int c[15] {};
-	int ExStr = 0;
-	for (int i = 0; i < 13; i++){
-		v[i] = (cards[i] & 0x0f);
-		c[v[i]]++;
-		if ((ExStr & (1 << (v[i] - 1))) == 0){ ExStr += 1 << (v[i] - 1); }
-		if (v[i] == 1){ 
-			v[i] = 14; 
-			c[v[i]]++; 
-			if ((ExStr & (1 << (v[i] - 1))) == 0){ ExStr += 1 << (v[i] - 1); }
-		}
-	}
-
-	if (STYPE == THREEFLUSH || STYPE == THREE_STRIGHTFLUSH || STYPE == BLACKDRAGON){
-		//basic sort
-		for (int i = 0; i < 13; i++){
-			int index = i;
-			for (int j = i; j < 13; j++){
-				if (cards[j] > cards[index]){
-					index = j;
-				}
-			}
-			BYTE ch = cards[i];
-			cards[i] = cards[index];
-			cards[index] = ch;
-		}
-	}
-	else{
-		//sort with val
-		for (int i = 0; i < 13; i++){
-			int index = i;
-			for (int j = i + 1; j < 13; j++){
-				if (v[j] > v[index]){
-					index = j;
-				}
-			}
-			BYTE ch = cards[i];
-			cards[i] = cards[index];
-			cards[index] = ch;
-
-			int in = v[i];
-			v[i] = v[index];
-			v[index] = in;
-		}
-
-		if (STYPE == THREESTRIGHT){
-			int TexPos[] = { 15873, 7936, 3968, 1984, 992, 496, 248, 124, 62, 8223 };
-			int ThPos[] = { 14337, 7168, 3584, 1792, 896, 448, 224, 112, 56, 28, 14, 8199 };
-
-			int sorted = 0;
-			BYTE cp[13] {};
-			memcpy(cp, cards, 13);
-			BYTE scards[13] {};
-
-			while (sorted < 13){
-				for (int i = 0; i < 12; i++){
-					if (((TexPos[i] & ExStr) == TexPos[i]) && i < 10){
-						int val = 14 - i;
-						int oc = 13 - sorted;
-						int times = 5;
-						for (int j = 0; j < oc; j++){
-							if (val == (cp[j] & 0x0f) || val == 14){
-								times--;
-								scards[sorted++] = cp[j];
-								c[val]--;
-								if (val == 14){ c[1]--; }
-								else if (val == 1){ c[14]--; }
-								if (c[val] == 0){ 
-									ExStr -= 1 << (val - 1);
-									if (val == 1){ ExStr -= 1 << 13; }
-									else if (val == 14){ ExStr -= 1; }
-								}
-								val--;
-								if (val == 1){ j = -1; }
-							}
-							if (times == 0){ break; }
-						}
-						DeleteSortedCards(scards, cp, sorted);
-						i--;
-						continue;
-					}
-
-					if ((ThPos[i] & ExStr) == ThPos[i]){
-						int val = 14 - i;
-						int oc = 13 - sorted;
-						int times = 3;
-						for (int j = 0; j < oc; j++){
-							if (val == (cp[j] & 0x0f) || val == 14){
-								times--;
-								scards[sorted++] = cp[j];
-								c[val]--;
-								if (val == 14){ c[1]--; }
-								else if (val == 1){ c[14]--; }
-								if (c[val] == 0){
-									ExStr -= 1 << (val - 1);
-									if (val == 1){ ExStr -= 1 << 13; }
-									else if (val == 14){ ExStr -= 1; }
-								}
-								val--;
-								if (val == 1){ j = -1; }
-							}
-							if (times == 0){ break; }
-						}
-						DeleteSortedCards(scards, cp, sorted);
-						i--;
-						continue;
-					}
-				}
-			}
-			memcpy(cards, scards, 13);
-		}
-	}
-}
-
-void CGameLogic::CompareCards(unsigned int pv1[], unsigned int pv2[], int re[]){
-	int in = 1;
-	int maxtype;
-
-	for (int i = 0; i < 3; i++){
-		if (pv1[i]>pv2[i]){
-			in = 1;
-			maxtype = (int)(pv1[i] >> 24);
-		}
-		else{
-			in = -1;
-			maxtype = (int)(pv2[i] >> 24);
-		}
-
-		if (maxtype == CT_THREE && i==0){ re[i] = 3 * in; }
-		else if (maxtype == CT_FULLHOUSE && i == 1){ re[i] = 2 * in; }
-		else if (maxtype == CT_FOUR && i == 1){ re[i] = 7 * in; }
-		else if (maxtype == CT_FOUR && i == 2){ re[i] = 5 * in; }
-		else if (maxtype == CT_FLUSHSTRIGHT && i == 1){ re[i] = 9 * in; }
-		else if (maxtype == CT_FLUSHSTRIGHT && i == 2){ re[i] = 5 * in; }
-		else { re[i] = 1 * in; }
-	}
-}
-
-int GetCount(int val){
-	val = val >> 1;
-	int count = 0;
-	while (val > 0){
-		count += val % 2;
-		val = val >> 1;
-	}
-	return count;
-}
-
-//ªÿÀ›3≤„
-bool JudgeThreeFlushStright(int Str[], int TexPos[], int ThPos[], int lay){
-	bool re = false;
-
-	if (lay < 3){
-		for (int i = 0; i < 10; i++){
-			for (int j = 0; j < 4; j++){
-				if ((Str[j] & TexPos[i]) == TexPos[i]){
-					Str[j] -= TexPos[i];
-					re = JudgeThreeFlushStright(Str, TexPos, ThPos, lay + 1);
-					Str[j] += TexPos[i];
-				}
-			}
-		}
-
-		return re;
-	}
-	else{
-		for (int i = 0; i < 12; i++){
-			for (int j = 0; j < 4; j++){
-				if ((Str[j] & ThPos[i]) == ThPos[i]){
-					return true;
-				}
-			}
-		}
-	}
-
-	return re;
-}
-
-int CalcNewStr(int Str, int Tex, int h[]){
-	Str -= Tex;
-	int index = 1;
-	while (Tex){
-		if (Tex % 2){ h[index]--; }
-		index++;
-		Tex = Tex >> 1;
-	}
-
-	for (int i = 0; i < 15; i++){
-		if (h[i]>0 && (Str&(1 << (i - 1))) == 0){
-			Str += (1 << (i - 1));
-		}
-	}
-
-	return Str;
-}
-
-bool JudgeThreeStright(int ExStr, int h[], int TexPos[], int ThPos[],int lay){
-	bool re = false;
-
-	if (lay < 3){
-		for (int i = 0; i < 10; i++){
-			if ((ExStr&TexPos[i]) == TexPos[i]){
-				ExStr = CalcNewStr(ExStr, TexPos[i], h);
-				return re = JudgeThreeStright(ExStr, h, TexPos, ThPos, lay + 1);
-			}
-		}
-	}
-	else{
-		for (int i = 0; i < 12; i++){
-			if ((ExStr&ThPos[i]) == ThPos[i]){
-				return true;
-			}
-		}
-	}
-
-	return re;
-}
-
-#define WEIGHT(a,b) (a<<24|b)
-unsigned int GetWeight(int Str[], int Exstr, int h[], int TexPos[], int ThPos[], int BlackCount){
-
-	//«‡¡˙
-	int FiveFlushCount = 0, ThreeFlushCount = 0;
-	for (int i = 0; i < 4; i++){
-		if (Str[i] == 0x3fff){
-			return BLACKDRAGON;
-		}
-
-		int in = GetCount(Str[i]);
-		if (in == 3){ ThreeFlushCount++; }
-		else if (in == 5){ FiveFlushCount++; }
-		else if (in == 8){ FiveFlushCount++; ThreeFlushCount++; }
-		else if (in == 10){ FiveFlushCount += 2; }
-	}
-
-	//¡˙
-	if (Exstr == 0x3fff){ return LOWDRAGON; }
-
-	// Æ∂˛ª ◊Â
-	int RoyalCount = 0, HighCount = 0, LowCount = 0;
-	for (int i = 14; i > 1; i--){
-		if (i > 10){ RoyalCount += h[i]; }
-		if (i > 8){ HighCount += h[i]; }
-		else { LowCount += h[i]; }
-	}
-	if (RoyalCount == 13){ return ROYALCARD; }
-
-	//»˝Õ¨ª®À≥
-	if (JudgeThreeFlushStright(Str, TexPos, ThPos, 1)){
-		return THREE_STRIGHTFLUSH;
-	}
-
-	//»˝∑÷ÃÏœ¬(»˝∏ˆ’®)
-	int PairCount = 0, ThreeCount = 0, BombCount = 0;
-	for (int i = 14; i > 1; i--){
-		if (h[i] == 4){ BombCount++; }
-		else if (h[i] == 3){ ThreeCount++; }
-		else if (h[i] == 2){ PairCount++; }
-
-		if (BombCount == 3){ return THREE_BOMB; }
-	}
-
-	//»´¥Û
-	if (HighCount == 13){ return ALLHIGH; }
-	//»´–°
-	if (LowCount == 13){ return ALLLOW; }
-	//¥’“ª…´
-	if (BlackCount == 13 || BlackCount == 0){ return ALLSAMECOLOR; }
-
-	//ÀƒÃ◊»˝Ãı£¨ŒÂ∂‘»˝Ãı£¨¡˘∂‘∞Î
-	if (ThreeCount == 4){ return FOUR_THREE; }
-	if (PairCount == 5 && ThreeCount == 1){ return FIVEPAIRSANDTHREE; }
-	if (PairCount == 6){ return SIXPAIRS; }
-
-	//»˝À≥◊”
-	if (JudgeThreeStright(Exstr, h, TexPos, ThPos, 1)){
-		return THREESTRIGHT;
-	}
-	//»˝Õ¨ª®
-	if (FiveFlushCount == 2 && ThreeFlushCount == 1){ return THREEFLUSH; }
-
-	return NORMAL_TYPE;
-}
-
-unsigned int GetWeight(int Str[], int Exstr, std::list<int> hs, int h[], int TexPos[], int Count){
-	std::list<int>::iterator ito;
-	int nw = 0;
-	//Õ¨ª®À≥
-	for (int i = 0; i < 4; i++){
-		for (int j = 0; j < 10; j++){
-			if ((Str[i] & TexPos[j]) == TexPos[j]){
-				nw = (14 - j) << 20;
-				return WEIGHT(CT_FLUSHSTRIGHT, nw);
-			}
-		}
-	}
-
-	//Õ¨ª®
-	for (int i = 0; i < 4; i++){
-		if (GetCount(Str[i]) == 5){
-			int in = Str[i];
-			int index = 0;
-			int k = 4;
-			in = in >> 1;
-			while (in){
-				if (in % 2){
-					nw += (index + 1) << k;
-					k += 4;
-				}
-				in = in >> 1;
-				index++;
-			}
-			return WEIGHT(CT_FLUSH, nw);
-		}
-	}
-
-	//À≥◊”
-	for (int i = 0; i < 10; i++){
-		if (Exstr == TexPos[i]){
-			nw = (14 - i) << 20;
-			return WEIGHT(CT_STRIGHT, nw);
-		}
-	}
-
-	//§Ô§´§È§Û
-	ito = hs.end();
-	ito--;
-	int count1 = 0, count2 = 0;
-	int col1 = 0, val1 = 0;
-	int val2 = 0;
-	count1 = *ito >> 8;
-	col1 = *ito % 16;
-	val1 = (*ito >> 4) % 16;
-	if (ito != hs.begin()){
-		ito--;
-		count2 = *ito >> 8;
-		val2 = (*ito >> 4) % 16;
-	}
-
-	nw = val1 << 20;
-	nw += val2 << 16;
-	int in = Count - count1 - count2;
-	int k = 0;
-	for (int i = 14; i > 0; i--){
-		if (k == in){ break; }
-		if (i != val1 && i != val2 && h[i] > 0){
-			nw += i << (12 - k * 4);
-			k++;
-		}
-	}
-	if (count1 == 3 && count2 > 1){
-		return WEIGHT(CT_FULLHOUSE, nw);
-	}
-	else if (count1 == 2 && count2 > 1){
-		return WEIGHT(CT_TWOPAIRS, nw);
-	}
-	else if (count1 == 4){
-		return WEIGHT(CT_FOUR, nw);
-	}
-	else if (count1 == 3)
-	{
-		return WEIGHT(CT_THREE, nw);
-	}
-	else if (count1 == 2){
-		return WEIGHT(CT_ONEPAIR, nw);
-	}
-	else{
-		return WEIGHT(CT_HIGHCARD, nw);
-	}
-}
-
-unsigned int CGameLogic::CalcWeight(const BYTE bCards[], int CardCount){
-	int Str[4];
-	memset(Str, 0, sizeof(int)* 4);
-	int ExStr = 0;
-	int h[15];
-	memset(h, 0, sizeof(int)* 15);
-	int BlackCount = 0;
-	std::list<int> hs;
-	std::list<int>::iterator ito;
-
-	int TexPos[] = { 15873, 7936, 3968, 1984, 992, 496, 248, 124, 62, 8223 };
-	int ThPos[] = { 14337, 7168, 3584, 1792, 896, 448, 224, 112, 56, 28, 14, 8199 };
-
-	for (int i = 0; i < CardCount; i++){
-		int col = bCards[i] >> 4;
-		int val = bCards[i] % 16;
-		if (val == 1){ val = 14; }
-
-		if (col == 1 || col == 3){ BlackCount++; }
-
-		h[val]++;
-		if (val == 14){ h[1]++; }
-
-		bool ex = false;
-		if (i == 0){ ex = true; }
-		for (ito = hs.begin(); ito != hs.end(); ito++){
-			if ((*ito & 0xf0) == (val << 4)){
-				*ito += 0x100;
-				if ((*ito & 0x0f) < col){
-					*ito = *ito - (*ito & 0x0f) + col;
-				}
-				ex = false;
-				break;
-			}
-			ex = true;
-		}
-		if (ex){ hs.push_back(0x100 + (val << 4) + col); }
-		hs.sort();
-
-		Str[col] += 1 << (val - 1);
-		if (val == 14){ Str[col] += 1; }
-
-		if ((ExStr&(1 << (val - 1))) == 0){ 
-			ExStr += 1 << (val - 1);
-			if (val == 14){ ExStr++; }
-		}
-	}
-	if (CardCount == 13){ return GetWeight(Str, ExStr, h, TexPos, ThPos, BlackCount); }
-	else{ return GetWeight(Str, ExStr, hs, h, TexPos, CardCount); }
-}
-
-bool CGameLogic::JudgeOrder(unsigned int Weight[]){
-	for (int i = 0; i < 2; i++){
-		if (Weight[i] > Weight[i + 1]){
-			return false;
-		}
-	}
-	return true;
-}
+#define ENDSELECTPAIR(st) RecallCards(card,ustw,CurSeq, Layer, st);}}}
 
 void DeleteUsedCards(std::list<BYTE> &cards, std::list<BYTE> us, BYTE Cur[], int La, int ha){
 	int st = 0;
@@ -587,7 +112,7 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 		}
 	}
 
-	//Õ¨ª®À≥
+	//ÂêåËä±È°∫
 	if (type > 8 && Layer < 3){
 		for (int i = 0; i < 4; i++){
 			for (int j = 0; j < 10; j++){
@@ -604,7 +129,7 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 		}
 	}
 
-	//Ã˙÷ß
+	//ÈìÅÊîØ
 	if (type > 7 && Layer < 3){
 		for (int i = 14; i > 1; i--){
 			if (h[i] == 15){
@@ -619,7 +144,7 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 		}
 	}
 
-	//∫˘¬´
+	//Ëë´Ëä¶
 	if (type > 6 && Layer < 3){
 		SELECTTHREE(0)
 			SELECTPAIR(3)
@@ -628,7 +153,7 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 			ENDSELECTTHREE(0)
 	}
 
-	//Õ¨ª®
+	//ÂêåËä±
 	if (type > 5 && Layer < 3){
 		for (int i = 0; i < 4; i++){
 			if (GetCount(Str[i]) >= 5){
@@ -675,14 +200,14 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 		}
 	}
 
-	//À≥◊”
+	//È°∫Â≠ê
 	if (type > 4 && Layer < 3){
 		for (int j = 0; j < 10; j++){
 			if ((ExStr & TexPos[j]) == TexPos[j]){
 				int v = 14 - j;
 				BYTE stcards[5][3] {};
 				int Count = 1;
-				//…Ë÷√ø®
+				//ËÆæÁΩÆÂç°
 				for (int i = 0; i < 5; i++){
 					int cc = 0;
 					for (int k = 0; k < 4; k++){
@@ -728,7 +253,7 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 		}
 	}
 
-	//»˝’≈
+	//‰∏âÂº†
 	if (type > 3){
 		SELECTTHREE(0)
 			BestSequence(card, We, Layer + 1, 4, CurSeq, BestSeq, BestWe);
@@ -738,7 +263,7 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 }}
 	}
 
-	//¡Ω∂‘
+	//‰∏§ÂØπ
 	if (type > 2 && Layer < 3){
 		for (int i = 14; i > 1; i--){
 			for (int j = 0; j < 6; j++){
@@ -777,7 +302,7 @@ void CGameLogic::BestSequence(std::list<BYTE> &card, unsigned int We[], int Laye
 		}
 	}
 
-	//∂‘◊”
+	//ÂØπÂ≠ê
 	if (type > 1 && Layer < 4){
 		int va1 = 1;
 		SELECTPAIR(0)
@@ -920,11 +445,11 @@ void CGameLogic::VeryForceSelectCard(BYTE bCards[], unsigned int we[]){
 	}
 	//not force
 	//-------------------------------------------------------------------------------------------------
-	//Àº¬∑  ”√”⁄»Œ∫Œ◊È∫œŒ Ã‚(¿®∫≈)
-	//Àº¬∑2£∫pos 2 > 1º¥ø…   allva=0x1fff- t1[i] x[10]= {all pos}  int va2=1<<x[i](i>4),va1=1<<x[i](i<5);
+	//ÊÄùË∑ØÈÄÇÁî®‰∫é‰ªª‰ΩïÁªÑÂêàÈóÆÈ¢ò(Êã¨Âè∑)
+	//ÊÄùË∑Ø2Ôºöpos 2 > 1Âç≥ÂèØ   allva=0x1fff- t1[i] x[10]= {all pos}  int va2=1<<x[i](i>4),va1=1<<x[i](i<5);
 	//int cc[5]={0,1,2,3,4};
-	//—°5∏ˆŒª÷√   :while(va2>va1)
-	//if(cc[i]==cc[i+1])(i=0,1,2,3,4À≥–Ú)  cc[i]=i; cc[i+1]++;
+	//ÈÄâ5‰∏™‰ΩçÁΩÆ   :while(va2>va1)
+	//if(cc[i]==cc[i+1])(i=0,1,2,3,4È°∫Â∫è)  cc[i]=i; cc[i+1]++;
 	//va1+=1<<x[cc[i]];(i=0,1,2,3,4)
 	//va2=allva-va1;
 	//-------------------------------------------------------------------------------------------------
