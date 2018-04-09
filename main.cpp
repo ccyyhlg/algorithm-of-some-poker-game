eabf19399fc32c186ad9854c0d34d18d502664a1
ccyyhlg <34642551+ccyyhlg@users.noreply.github.com>
2018-04-09 10:52:16 +0800

    quiz(it's not good code)
    为了传授人生经验，长者给HK瑞珀特和MC艾迪特出了一道题目。他从2到n中选出两个不同的整数a和b，a＜b，把和告诉瑞珀特，把积告诉艾迪特。
         瑞珀特说：我虽然不能确定这两个数是什么，但是我肯定你也不知道这两个数是什么。 艾迪特说：我本来的确不知道，但是听你这么一说，我现在能够确定这两个数字了。 瑞珀特说：既然你这么说，我现在也知道这两个数字是什么了。 Naive的出题人一脸懵逼，所以请你帮忙求两个数字。

---
+++ main.cpp
@@ -0,0 +1,324 @@
+#include <iostream>
+#include <list>
+#include <time.h>
+#define MAXNUM 99
+
+bool PrimeNum(std::list<int> prime, int v){
+	std::list<int>::iterator ito = prime.begin();
+	while ((*ito) <= v){
+		if (ito == prime.end()){ return false; }
+		if ((*ito) < v){ ito++; }
+		else if ((*ito) == v){ return true; }
+	}
+	return false;
+}
+
+bool TwoAddNumNotPrime(std::list<int> prime, int num){
+	std::list<int>::iterator ito = prime.begin();
+	for (;; ito++){
+		int v = num - (*ito);
+		if (v < (*ito)){ return true; }
+		if (PrimeNum(prime, v)){ return false; }
+	}
+}
+
+bool TwoMulNumOnlyOnePos(std::list<int> prime, int num){
+	std::list<int>::iterator ito = prime.begin();
+	int cc = 0;
+	int mi, ma;
+	for (; ito!=prime.end(); ito++){
+		int v = num / (*ito);
+		if (v < 2){ break; }
+		if (v * (*ito) == num){
+			if (TwoAddNumNotPrime(prime, (*ito) + v)){
+				cc++;
+				mi = (v>(*ito)) ? *ito : v;
+				ma = (v<(*ito)) ? *ito : v;
+			}
+		}
+		if (cc > 1){ return false; }
+	}
+	if (cc == 1){ 
+		std::cout << mi << " " << ma << " "; 
+		return true; }
+	else return false;
+}
+
+bool TwoAddNumOnlyOnePos(std::list<int> prime, int num){
+	int cc = 0;
+	int mi, ma;
+	for (int i = 2; i < num;i++){
+		int v = num - i;
+		if (v < i){ break; }
+		if (TwoMulNumOnlyOnePos(prime, i * v)){
+			cc++;
+			mi = (v > i) ? i : v;
+			ma = (v < i) ? i : v;
+		}
+		if (cc > 1){ return false; }
+	}
+	if (cc == 1){ 
+		std::cout << mi << " " << ma << " "; 
+		return true; }
+	else return false;
+}
+
+bool JudgeNumInList(std::list<int> &listnum, int v){
+	std::list<int>::iterator ito = listnum.begin();
+
+	while (ito != listnum.end()){
+		if ((*ito) == v){
+			return true;
+		}
+		else if ((*ito) > v){
+			return false;
+		}
+		ito++;
+	}
+	return false;
+}
+
+void delete2prime(std::list<int> &addnum, std::list<int>&mulnum, std::list<int> &prime){
+	std::list<int> TwoPrimeAdd;
+	std::list<int> TwoPrimeMul;
+	for (std::list<int>::iterator ito1 = prime.begin(); (*ito1) < MAXNUM; ito1++){
+		for (std::list<int>::iterator ito2 = ito1; (*ito2) < MAXNUM; ito2++){
+			TwoPrimeAdd.push_back((*ito1) + (*ito2));
+			TwoPrimeMul.push_back((*ito1) * (*ito2));
+		}
+	}
+	TwoPrimeAdd.sort();
+	TwoPrimeAdd.unique();
+
+	std::list<int>::iterator twomul;
+	for (int num1 = 2; num1 < MAXNUM; num1++){
+		/*bool pri = JudgeNumInList(prime, num1);
+		if (pri){
+			for (int num2 = num1 + 1; num2 < MAXNUM; num2 ++){
+				if (!JudgeNumInList(prime, num2)){
+					mulnum.push_back(num1 * num2);
+				}
+			}
+		}
+		else{
+			for (int num2 = num1; num2 < MAXNUM; num2++){
+				mulnum.push_back(num2 * num1);
+			}
+		}*/
+		twomul = TwoPrimeMul.begin();
+		for (int num2 = num1; num2 < MAXNUM; num2++){
+			if (twomul == TwoPrimeMul.end()){ twomul--; }
+			else if ((num1*num2) == (*twomul)){ twomul++; continue; }
+			else if ((num1*num2)>(*twomul)){ twomul++; num2--; }
+			else{ mulnum.push_back((num1*num2)); }
+		}
+	}
+	mulnum.sort();
+
+	std::list<int>::iterator twoadd = TwoPrimeAdd.begin();
+	for (int num = 4; num < 2 * MAXNUM; num++){
+		if (twoadd == TwoPrimeAdd.end()){ twoadd--; }
+		if (num == (*twoadd)){ twoadd++; continue; }
+		else{ addnum.push_back(num); }
+	}
+
+	std::list<int> AllPrimePos;
+	twoadd = TwoPrimeAdd.begin();
+	while (twoadd != TwoPrimeAdd.end()){
+		for (int v1 = (*twoadd)/2; v1 > 1; v1--){
+			int v2 = *twoadd - v1;
+			AllPrimePos.push_back(v2*v1);
+		}
+		twoadd++;
+	}
+	AllPrimePos.sort();
+
+	std::list<int>::iterator alp = AllPrimePos.begin();
+	for (twomul = mulnum.begin(); twomul != mulnum.end();){
+		if (alp == AllPrimePos.end()){ break; }
+		if ((*twomul) < (*alp)){ twomul++; }
+		else if ((*twomul)>(*alp)){ alp++; }
+		else
+		{
+			twomul = mulnum.erase(twomul);
+			alp++;
+		}
+	}
+
+	for (std::list<int>::iterator ito1 = mulnum.begin(); ito1 != mulnum.end(); ){
+		std::list<int>::iterator ito2 = ito1;
+		ito2++;
+		if (ito2 == mulnum.end()){ break; }
+		bool same = false;
+		while ((*ito1) == (*ito2)){ ito2 = mulnum.erase(ito2); same = true;}
+		if (same){ ito1 = mulnum.erase(ito1); }
+		else{ ito1++; }
+	}
+}
+
+void OnlyOnePossible(std::list<int> &addnum, std::list<int>&mulnum, std::list<int> &prime){
+	std::list<int>::iterator p1 = prime.begin();
+
+	std::list<int>::iterator ito = mulnum.begin();
+	std::list<int>::iterator p;
+	
+	while (ito != mulnum.end()){
+		p = prime.begin();
+		int v = (*ito) / (*p);
+		int c = 0;
+		while ((*p) < MAXNUM && v > 1 ){
+			if ((*p)*v != (*ito)){
+				p++;
+				v = (*ito) / (*p);
+				continue;
+			}
+			else{
+				if (JudgeNumInList(addnum, (*p) + v)){
+					c++;
+				}
+				p++;
+				v = (*ito) / (*p);
+			}
+			if (c > 1){ break; }
+		}
+		if (c != 1){ ito = mulnum.erase(ito); }
+		else{ ito++; }
+	}
+}
+
+void OnlyOnePosInAdd(std::list<int> &addnum, std::list<int>&mulnum, std::list<int> &prime){
+	std::list<int>::iterator ito = addnum.begin();
+
+	while (ito != addnum.end()){
+		int c = 0;
+		int mi, ma;
+		for (int i = 2; i < (*ito) / 2; i++){
+			int j = (*ito) - i;
+			if (JudgeNumInList(mulnum, i*j)){
+				c++;
+				if (c > 1){ break; }
+				mi = i < j ? i : j;
+				ma = i < j ? j : i;
+			}
+		}
+		if (c == 1){ std::cout << mi << " " << ma << " " << std::endl; }
+		if (c != 1){ ito = addnum.erase(ito); }
+		else{ ito++; }
+	}
+}
+
+std::list<int> FirstAlgothm(std::list<int> prime){
+	std::list<int>::iterator ito;
+	std::list<int> fi;
+	for (int i = 8; i < MAXNUM; i++){
+		bool sui = true;
+		ito = prime.begin();
+		while ((*ito) < i){
+			int v = i - (*ito);
+			if (*ito > v){  break; }
+			std::list<int>::iterator ito2 = prime.end();
+			ito2--;
+			bool dp = false;
+			while (*ito2 >= v){
+				if (*ito2 == v){
+					dp = true;;
+					break;
+				}
+				else if (*ito2 < v){
+					break;
+				}
+				else{
+					ito2--;
+				}
+			}
+			if (dp){ sui = false; break; }
+			ito++;
+		}
+		if (sui){ fi.push_back(i); }
+	}
+	return fi;
+}
+
+std::list<int> SecondAlgothm(std::list<int> prime){
+	std::list<int>::iterator ito=prime.begin();
+	std::list<int> se;
+
+	for (int i = 2; i < MAXNUM; i++){
+		ito = prime.begin();
+		int v = i / (*ito);
+		if (v*(*ito) == i){
+
+		}
+	}
+	return se;
+}
+
+int main(){
+	int s[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+	std::list<int> prime;
+	std::list<int> addnum;
+	std::list<int> mulnum;
+	for (int i = 0; i < 10; i++){ prime.push_back(s[i]); }
+
+	/*for (int i = 4; i < 2 * MAXNUM; i++){ addnum.push_back(i); }
+	for (int i = 2; i < MAXNUM; i++){
+		for (int j = i; j < MAXNUM; j++){
+			mulnum.push_back(i*j);
+		}
+	}
+	mulnum.sort();
+	mulnum.unique();*/
+
+	long st, en;
+	st = clock();
+	for (int num = 31; num < MAXNUM*MAXNUM; num++){
+		std::list<int>::iterator ito = prime.begin();
+		
+		bool pri = true;
+		while (ito!=prime.end())
+		{
+			int ne = num / (*ito);
+			if (ne < (*ito)){ break; }
+			if (ne * (*ito) == num){
+				pri = false;
+				break;
+			}
+			ito++;
+		}
+		if (pri){ prime.push_back(num); }
+	}
+	en = clock();
+	std::cout << "prime:" << en - st << std::endl;
+
+	st = clock();
+	//first sentence 
+	delete2prime(addnum, mulnum, prime);
+	en = clock();
+	std::cout << "p1:" << en - st << std::endl;
+
+	//st = clock();
+	////second sentence
+	//OnlyOnePossible(addnum, mulnum, prime);
+	//en = clock();
+	//std::cout << "p2:" << en - st << std::endl;
+
+	st = clock();
+	//third sentence
+	OnlyOnePosInAdd(addnum, mulnum, prime);
+	en = clock();
+	std::cout << "p3:" << en - st << std::endl;
+	/*std::list<int> first;
+	first = FirstAlgothm(prime);
+	std::list<int> second;
+	second = SecondAlgothm(prime);
+
+	std::list<int>::iterator ito1 = first.begin();
+	std::list<int>::iterator ito2 = second.begin();
+
+	for (int i = 4; i < MAXNUM; i++){
+		if (TwoMulNumOnlyOnePos(prime, i)){
+			std::cout << i << " " << std::endl;
+		}
+	}*/
+	return 0;
+}
